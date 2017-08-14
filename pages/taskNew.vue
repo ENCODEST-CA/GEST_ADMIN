e30c72b275d521f217fb894750783a073f17f49e
unknown <yohanysturiz@gmail.com>
2017-08-14 17:00:36 -0430

    Modificaciones en el formulario, cambio de nomenclatura

--- pages/taskNew.vue
+++ pages/taskNew.vue
@@ -5,19 +5,18 @@
 				v-card-media( class="white--text" height="50px" style="background-color:#2196f3;")
 					v-container(fill-height fluid)
 						v-layout(fill-height)
-							v-flex(xs12 flexbox class="centrado-porcentual")
+							v-flex(xs12 flexbox class="centered-percentege")
 								span(class="headline") Nueva tarea Express				
-				v-layout(row wrap)
-					v-card-title
+				v-layout(row wrap class="form-border")
 						v-flex(xs6 sm6 md6 class="my-3 camp")
 							v-layout(row wrap)
 								v-flex(xs12)
 									v-text-field(
 										label="Titulo"
 										prepend-icon="account_circle"
-										v-model="Titulo"
+										v-model="form.title"
 										max="50"
-										@click.native="informacion('Acá deberas colocar el titulo de tu tarea, Recuerda no debe ser tan largo pero muy descriptivo.')"
+										@click.native="information('Acá deberas colocar el titulo de tu tarea, Recuerda no debe ser tan largo pero muy descriptivo.')"
 										single-line)
 						v-flex(xs6 sm6 md6 class="my-3 camp")
 							v-layout(row wrap)
@@ -25,24 +24,26 @@
 									v-select(
 										label="Responsable"
 										prepend-icon="portrait"
-										v-model="responsable"
+										v-model="form.responsable"
 										:items="options"
 										max="50"
-										@click.native="informacion('Deberas elegir el responsable de la tarea por registrar.')"
+										@click.native="information('Deberas elegir el responsable de la tarea por registrar.')"
 										single-line
 										)
-						v-flex(xs12 class="camp")
-								v-text-field(
-									label="Descripción"
-									prepend-icon="comment"
-									v-model="descripcion"
-									max="1000"
-									counter
-									@click.native="informacion('Acá deberas añadir una descripcion que sirva de ayuda.')"
-									single-line)
+						v-flex(xs12 sm12 md12 class="my-3 camp")
+							v-layout(row wrap)
+								v-flex(xs12 class="camp")
+										v-text-field(
+											label="Descripcion"
+											prepend-icon="comment"
+											v-model="form.description"
+											counter
+											max="1000"
+											@click.native="information('Aca colocas toda la informacion que sea de interes sobre la tarea por crear.')"
+											single-line)
 				v-slide-y-transition
 					v-card-text(v-show="show")
-						v-alert(info icon="bookmark_border" v-bind:value="true") {{ mensajes.CampoMarcado }}
+						v-alert(info icon="bookmark_border" v-bind:value="true") {{ message.markedField }}
 				v-card-actions    
 					v-btn(flat class="orange--text") Guardar
 					v-btn(flat class="orange--text") Cancelar
@@ -52,6 +53,11 @@
 	data () {
 	  return {
 		show: false,
+		form:{
+			title: null,
+			responsable: null,
+			description: null,
+		},
 		options:[{
 			value:1,
 			text:'Pedro Perez'
@@ -62,14 +68,14 @@
 			value:3,
 			text:'Hugo Chavez'
 		}],
-		mensajes: {
-			CampoMarcado: ""
+		message: {
+			markedField: ""
 		},
 	  }
 	},
 	methods:{
-		informacion: function (mensaje){
-			this.mensajes.CampoMarcado = mensaje
+		information: function (message){
+			this.message.markedField = message
 			this.show = true
 		}
 	}
@@ -80,11 +86,13 @@
 	.margin-form
 		margin: 0px 20px 0px 20px
 	.camp
-		margin: 0px 0px 0px 0px 
+		margin: 0px 0px 0px 0px
+	.form-border
+		padding: 25px 25px 0px 25px
 
-	.centrado-porcentual
+	.centered-percentege
 	    position: absolute
-	    left: 13%
+	    left: 130px
 	    top: 50%
 	    transform: translate(-50%, -50%)
 	    -webkit-transform: translate(-50%, -50%)
